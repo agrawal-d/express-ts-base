9438dd87843df71cd4b185b089f3683986cb30eb
Divyanshu Agrawal <agrawal-d@outlook.com>
2022-02-13 10:07:57 +0530

    Initial commit.

---
+++ src/middleware/validate.ts
@@ -0,0 +1,16 @@
+import { validationResult } from "express-validator";
+import { NextFunction, Request, Response } from "express";
+
+export default function validate(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    return next();
+}
